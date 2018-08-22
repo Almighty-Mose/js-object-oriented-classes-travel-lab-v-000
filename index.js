44c151bd434f41b0fedb2ceed491d72e52d2694b
Branden Mosley <branden.mosley1337@gmail.com>
2018-08-22 19:27:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ class Route {
     this.endingLocation = endingLocation;
   }
   
-  avenueToInteger(avenue) {
-    return eastWest.indexOf(avenue);
+  avenueToInteger(avenue){
+    return eastWest.indexOf(avenue)
   }
   
-  blocksTravelled() {
-    let horizontalDistance = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal);
-    let verticalDistance = this.avenueToInteger(this.endingLocation.vertical) - this.avenueToInteger(this.beginningLocation.vertical);
-    return (Math.abs(horizontalDistance) + Math.abs(verticalDistance));
+  blocksTravelled(){
+    let horizontalDistance = this.avenueToInteger(this.endingLocation.horizontal) - this.avenueToInteger(this.beginningLocation.horizontal)
+    let verticalDistance = this.endingLocation.vertical - this.beginningLocation.vertical
+    return (Math.abs(horizontalDistance) + Math.abs(verticalDistance))
   }
 }
